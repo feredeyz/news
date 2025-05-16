6b928209d8a64504e0283cb3cdf3162a5536a768
feredeyz <legocitiy@gmail.com>
2025-05-17 01:32:59 +0300

    omagat i beat taidal weiv omagat gg pochti ves front gotov

--- news/newsapp/forms.py
+++ news/newsapp/forms.py
@@ -1,17 +1,17 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=30, min_length=3)
-    password = forms.CharField(max_length=30, min_length=3, widget=forms.PasswordInput())
+    username = forms.CharField(max_length=30, min_length=3, label='', widget=forms.TextInput(attrs={"placeholder": "Имя пользователя", "id": "username"}))
+    password = forms.CharField(max_length=30, min_length=3, label='', widget=forms.PasswordInput(attrs={"placeholder": "Пароль", "id": "password"}))
 
 class RegistrationForm(forms.Form):
-    username = forms.CharField(max_length=30, min_length=3)
-    password = forms.CharField(max_length=30, min_length=3, widget=forms.PasswordInput())
-    confirm_password = forms.CharField(max_length=30, min_length=3, widget=forms.PasswordInput())
+    username = forms.CharField(max_length=30, min_length=3, label='', widget=forms.TextInput(attrs={"placeholder": "Имя пользователя", "id": "username"}))
+    password = forms.CharField(max_length=30, min_length=3, label='', widget=forms.PasswordInput(attrs={"placeholder": "Пароль", "id": "password"}))
+    confirm_password = forms.CharField(max_length=30, min_length=3, label='', widget=forms.PasswordInput(attrs={"placeholder": "Подтвердите пароль", "id": "confirm_password"}))
 
 class AddNewsForm(forms.Form):
-    title = forms.CharField(max_length=100, label='', widget=forms.TextInput(attrs={'placeholder': 'Заголовок'}))
-    content = forms.CharField(label='', widget=forms.Textarea(attrs={'placeholder': "Новость"}))
+    title = forms.CharField(max_length=100, label='', widget=forms.TextInput(attrs={'placeholder': 'Заголовок', "id": 'title'}))
+    content = forms.CharField(label='', widget=forms.Textarea(attrs={'placeholder': "Новость", 'id': 'content'}))
 
 class SearchNewsForm(forms.Form):
-    title = forms.CharField(max_length=100, label='', widget=forms.TextInput(attrs={"placeholder": "Заголовок новости"}))
+    title = forms.CharField(max_length=100, label='', widget=forms.TextInput(attrs={"placeholder": "Заголовок новости", "id": "title"}))
